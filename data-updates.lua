4e78153de4d9fd1a13afd351649184ad4541624d
Em Karisch <emii.khaos@gmail.com>
2026-07-13 18:08:50 +0200

    refactor: migrate to khaoscircuitry

--- data-updates.lua
+++
@@ -1,5 +0,0 @@
--- Better subgroup placement with SchallCircuitGroup
-require("data-updates.schall-circuit-group")
-
--- Replace power switch with arithmetic combinator in recipe
-require("data-updates.recipe-changes")
--- data-updates/schall-circuit-group.lua
+++
@@ -1,3 +0,0 @@
-if mods["SchallCircuitGroup"] then
-  data.raw.item["belt-arithmetic-combinator"].subgroup = "circuit-input"
-end
